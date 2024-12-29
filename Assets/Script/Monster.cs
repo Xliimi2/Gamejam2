2bc36191ec25722051350fe52b0c3103dd355d17
Alhanouf <111021921+Xliimi2@users.noreply.github.com>
2024-12-29 18:29:02 +0300

    .

--- Assets/Script/Monster.cs
+++ Assets/Script/Monster.cs
@@ -6,16 +6,7 @@ public class Monster : NetworkBehaviour
     private int hitCount = 0; // عدد الطلقات التي أصابت الوحش
     private Collider2D monsterCollider;
 
-    void Start()
-    {
-        monsterCollider = GetComponent<Collider2D>();
-
-        if (!IsOwner)
-        {
-            gameObject.SetActive(false); // إخفاء الوحش عن اللاعبين الآخرين
-        }
-    }
-
+   
     public void TakeDamage()
     {
         if (IsOwner)
@@ -23,17 +14,10 @@ public class Monster : NetworkBehaviour
             hitCount++;
             if (hitCount >= 3)
             {
-                DestroyMonsterServerRpc(); // إزالة الوحش بعد ثلاث طلقات
             }
         }
     }
 
-    [ServerRpc]
-    private void DestroyMonsterServerRpc()
-    {
-        DestroyMonsterClientRpc(); // مزامنة الإزالة مع جميع اللاعبين
-        Destroy(gameObject);       // إزالة الوحش من السيرفر
-    }
 
     [ClientRpc]
     private void DestroyMonsterClientRpc()
